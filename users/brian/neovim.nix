853a88e0805d9a180f487e8871275431e954096a
Brian Semrad <bjsemrad@gmail.com>
2024-01-10 21:04:36 -0600

    Desktop Refactor

--- users/brian/neovim.nix
+++ users/brian/neovim.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, , ... }:
+{ config, pkgs, ... }:
 
 {
   programs.neovim = {
     enable = true;
     extraConfig = ":luafile ~/.config/nvim/lua/init.lua";
   };
-  
+
   xdg.configFile.nvim = {
     source = ./nvim_config;
     recursive = true;
