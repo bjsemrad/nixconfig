ec4f7e0a4a03b349de8edbe05826e9673a9e1eb7
Brian Semrad <bjsemrad@gmail.com>
2024-01-12 22:47:00 -0600

    Starting Hyprland

--- users/brian/hyprland/swayidle/default.nix
+++ users/brian/hyprland/swayidle/default.nix
@@ -4,21 +4,24 @@
     enable = true;
     events = [
       {
-        event = "timeout 300";
-        command = "swaylock -f --clock";
-      }
-      {
-        event = "timeout 305";
-        command = "hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on";
-      }
-      {
-        event = "timeout 600";
-        command = "systemctl suspend' resume 'hyprctl dispatch dpms on";
-      }
-      {
         event = "before-sleep";
         command = "swaylock -f --clock";
       }
     ];
-  };
-}
+
+    timeouts = [
+      {
+        timeout = 300;
+        command = "swaylock -f --clock";
+      }
+      {
+        timeout = 305;
+        command = "hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on";
+      }
+      {
+        timeout = 600;
+        command = "systemctl suspend' resume 'hyprctl dispatch dpms on";
+      }
+    ]
+      };
+  }
