dd1dc276709e11cab09bceb6c0589f1bb3384bcb
Brian Semrad <bjsemrad@gmail.com>
2024-01-14 10:18:17 -0600

    Rofi Theme

--- users/brian/modules/shell/default.nix
+++ users/brian/modules/shell/default.nix
@@ -8,8 +8,8 @@ let
     enable = true;
     shellAliases = {
       #homeupdate = "home-manager switch --flake ${nixconfigdir}";
-      nixupdate = "cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
-      nixswitch = "cd ${config.home.homeDirectory}/${nixconfigdir} && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
       nixgc = "nix-store --gc";
       # recommeneded to sometimes run as sudo to collect additional garbage
       nixgcd = "sudo nix-collect-garbage -d";
