3037abb4590c0c751022f0f6742ac5464482ab2c
Brian Semrad <bjsemrad@gmail.com>
2024-01-14 18:37:19 -0600

    Start Desktop config

--- users/brian/modules/hyprland/default.nix
+++ users/brian/modules/hyprland/default.nix
@@ -20,11 +20,17 @@
     name = "Bibata-Modern-Classic";
     size = 22;
   };
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 
   gtk = {
     enable = true;
     theme = {
-      name = "Adwaita:dark";
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
     };
 
     gtk3.extraConfig = {
