abae64e91e1a8d7a8df841397fb5393d4c2f9781
Brian Semrad <bjsemrad@gmail.com>
2024-01-14 21:49:14 -0600

    Dashboard config

--- users/dash/default.nix
+++ users/dash/default.nix
@@ -18,38 +18,6 @@ in
       "compose.yaml".source = ./compose.yaml;
     };
 
-    programs.zsh = {
-      enable = true;
-      shellAliases = {
-        #homeupdate = "home-manager switch --flake ${nixconfigdir}";
-        nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
-        nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
-        nixgc = "nix-store --gc";
-        # recommeneded to sometimes run as sudo to collect additional garbage
-        nixgcd = "sudo nix-collect-garbage -d";
-        ga = "git add";
-        gba = "git branch -a";
-        gc = "git commit -v";
-        gcb = "git checkout -b";
-        gcmsg = "git commit -m";
-        gl = "git pull";
-        gp = "git push";
-        gco = "git checkout";
-        gst = "git status";
-      };
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true; #OLD enableSyntaxHighlighting = true;
-    };
-
-
-
-    programs.git = {
-      enable = true;
-      userName = "Brian Semrad";
-      userEmail = "bjsemrad@gmail.com";
-    };
-
     sessionVariables = {
       # EDITOR = "emacs";
     };
@@ -62,6 +30,39 @@ in
     # release notes.
     stateVersion = "23.11"; # Please read the comment before changing.
   };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      #homeupdate = "home-manager switch --flake ${nixconfigdir}";
+      nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixgc = "nix-store --gc";
+      # recommeneded to sometimes run as sudo to collect additional garbage
+      nixgcd = "sudo nix-collect-garbage -d";
+      ga = "git add";
+      gba = "git branch -a";
+      gc = "git commit -v";
+      gcb = "git checkout -b";
+      gcmsg = "git commit -m";
+      gl = "git pull";
+      gp = "git push";
+      gco = "git checkout";
+      gst = "git status";
+    };
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true; #OLD enableSyntaxHighlighting = true;
+  };
+
+
+
+  programs.git = {
+    enable = true;
+    userName = "Brian Semrad";
+    userEmail = "bjsemrad@gmail.com";
+  };
+
 
   # environment.
   programs.direnv.enable = true;
