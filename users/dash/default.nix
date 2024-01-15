fef8751c52c6fe0e5131f1bf7414820c8b98107e
Brian Semrad <bjsemrad@gmail.com>
2024-01-14 21:22:28 -0600

    Dashboard config

--- users/dash/default.nix
+++ users/dash/default.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 
+let
+  nixconfigdir = "nixconfig";
+in
 {
+
   imports = [
   ];
-
 
   home = {
     username = "dash";
@@ -11,6 +14,32 @@
     packages = with pkgs; [
 
     ];
+
+    programs.zsh = {
+      enable = true;
+      shellAliases = {
+        #homeupdate = "home-manager switch --flake ${nixconfigdir}";
+        nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+        nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+        nixgc = "nix-store --gc";
+        # recommeneded to sometimes run as sudo to collect additional garbage
+        nixgcd = "sudo nix-collect-garbage -d";
+        ga = "git add";
+        gba = "git branch -a";
+        gc = "git commit -v";
+        gcb = "git checkout -b";
+        gcmsg = "git commit -m";
+        gl = "git pull";
+        gp = "git push";
+        gco = "git checkout";
+        gst = "git status";
+      };
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true; #OLD enableSyntaxHighlighting = true;
+    };
+
+
 
     programs.git = {
       enable = true;
