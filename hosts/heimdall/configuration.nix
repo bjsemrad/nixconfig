08959120a97f84ab37439563798d105befa0b1f2
Brian Semrad <bjsemrad@gmail.com>
2024-01-24 11:35:46 -0600

    Setup Wormhole initil

--- hosts/heimdall/configuration.nix
+++ hosts/heimdall/configuration.nix
@@ -66,6 +66,10 @@
     ];
   };
 
+   users.users.root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxG6NiEQZOEJiqpEDXg/eERqe71XNqnNLlI7VaOGqch bjsemrad@gmail.com"
+    ];
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
