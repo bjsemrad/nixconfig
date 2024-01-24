fca7dc68b7ca30b12e32c869b4f1b388a008dae8
Brian Semrad <bjsemrad@gmail.com>
2024-01-23 20:22:53 -0600

    Hyprland latest

--- users/brian/modules/shell/default.nix
+++ users/brian/modules/shell/default.nix
@@ -10,7 +10,7 @@ let
       #homeupdate = "home-manager switch --flake ${nixconfigdir}";
       nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
       nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
-      nixgc = "nix-store --gc";
+      nixgc = "sudo nix-store --gc";
       # recommeneded to sometimes run as sudo to collect additional garbage
       nixgcd = "sudo nix-collect-garbage -d";
       ga = "git add";
