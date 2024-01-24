8c98a2af4756c16bb412d09ad1e52d513472b851
Brian Semrad <bjsemrad@gmail.com>
2024-01-24 11:37:36 -0600

    Setup Wormhole initil

--- hosts/wormhole/configuration.nix
+++ hosts/wormhole/configuration.nix
@@ -62,6 +62,10 @@
     ];
   };
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxG6NiEQZOEJiqpEDXg/eERqe71XNqnNLlI7VaOGqch bjsemrad@gmail.com"
+  ];
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
