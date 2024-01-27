4adba3c918de25d9d0c514e7e5bc6e4617fbc2cd
Brian Semrad <bjsemrad@gmail.com>
2024-01-27 13:56:28 -0600

    Add external drive to odin

--- hosts/odin/hardware-configuration.nix
+++ hosts/odin/hardware-configuration.nix
@@ -52,7 +52,7 @@
     device = "/dev/disk/by-uuid/dab14479-8c57-4f41-aac0-4d78005dbedd";
     fsType = "btrfs";
     options = [ "compress=zstd" "noatime" "nosuid" "nodev" "nofail" "x-gvfs-show" ];
-    label = "games";
+    label = "gamestorage";
   };
 
   swapDevices = [{ device = "/swap/swapfile"; }];
