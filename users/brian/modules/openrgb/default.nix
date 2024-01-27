68ab34f6997475d1c55ad6e2430b5549a8c831fd
Brian Semrad <bjsemrad@gmail.com>
2024-01-26 21:41:22 -0600

    OpenRGB

--- users/brian/modules/openrgb/default.nix
+++ users/brian/modules/openrgb/default.nix
@@ -1,6 +1,5 @@
 { lib, osConfig, ... }:
-#lib.mkIf (osConfig.networking.hostName == "odin") 
-{
+lib.mkIf (osConfig.networking.hostName == "odin")  {
   home.file = {
     ".config/OpenRGB/Nix.orp".source = ./Nix.orp;
     ".config/OpenRGB/sizes.ors".source = ./sizes.ors;
