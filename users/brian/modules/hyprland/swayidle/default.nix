201d2b494d409d023b2e79997afa03a2ac45ae2e
Brian Semrad <bjsemrad@gmail.com>
2024-01-27 15:37:17 -0600

    Idle tweaks with desktop

--- users/brian/modules/hyprland/swayidle/default.nix
+++ users/brian/modules/hyprland/swayidle/default.nix
@@ -1,4 +1,4 @@
-{  pkgs, ... }:
+{ pkgs, osConfig, ... }:
 {
   services.swayidle = {
     enable = true;
@@ -19,11 +19,14 @@
         command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
-      {
-        timeout = 600;
-        command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.systemd}/bin/systemctl suspend";
-        resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-      }
-    ];
+    ] ++ (
+      if (osConfig.networking.hostName == "thor") then
+        [{
+          timeout = 600;
+          command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.systemd}/bin/systemctl suspend";
+          resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+        }]
+      else [ ]
+    );
   };
 }
