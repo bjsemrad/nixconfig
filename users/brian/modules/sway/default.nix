17cba1e9e19bb10e9259b412adacbe02db4f6895
Brian Semrad <bjsemrad@gmail.com>
2024-02-01 21:16:28 -0600

    Starting to theme Sway, still prefer Hyprland

--- users/brian/modules/sway/default.nix
+++ users/brian/modules/sway/default.nix
@@ -1,10 +1,27 @@
 {
   wayland.windowManager.sway = {
     enable = true;
-    config = rec {
+    xwayland = true;
+    config = {
       modifier = "Mod1";
       # Use kitty as default terminal
       terminal = "alacritty";
+      output = rec {
+        eDP-1 = {
+          scale = "1.175";
+        };
+      };
+      input = { "type:touchpad" = { tap = "enabled"; natural_scroll = "enabled"; }; };
+      bars = [
+        {
+          command = "waybar";
+        }
+      ];
+      input = {
+        "type:keyboard" = {
+          xkb_options = "ctrl:nocaps";
+        };
+      };
       # startup = [
       #   # Launch Firefox on start
       #   {command = "firefox";}
