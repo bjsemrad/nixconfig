9fd82f8518f5e8434ce377854c4ae27943d062bf
Brian Semrad <bjsemrad@gmail.com>
2024-02-20 19:48:49 -0600

    Hyprlock and Idle flake addition, not yet ready for prime time

--- users/brian/modules/hyprland/hypridle/default.nix
+++ users/brian/modules/hyprland/hypridle/default.nix
@@ -1,4 +1,11 @@
-{ config, inputs, ...}: 
+{ config, inputs, ...}:
+let
+  hypridle = inputs.hypridle;
+in 
 {
+  #imports = [
+  #  hypridle.homeManagerModules.default
+  #];
+
   #services.hypridle.enable = true;
 }
--- users/brian/modules/hyprland/hyprlock/default.nix
+++ users/brian/modules/hyprland/hyprlock/default.nix
@@ -7,5 +7,20 @@ in
     hyprlock.homeManagerModules.default
   ];
 
-  programs.hyprlock.enable = true;
+  programs.hyprlock = {
+    enable = true;
+    backgrounds = [{
+      path = "/home/brian/.config/hypr/nix-purple.png";
+    }];
+    input_field = {
+      placeholder_text = "Password....";
+      size = {
+          width = 200;
+          height = 50;
+      };
+      outline_thickness = 3;
+      halign = "center";
+      valign = "bottom";
+    };
+  };
 }
