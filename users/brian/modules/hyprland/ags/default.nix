018e4f29e9062bd0c87a16b3f962a8a967fc2e2a
Brian Semrad <bjsemrad@gmail.com>
2024-06-24 14:23:52 -0500

    Reset ags

--- users/brian/modules/hyprland/ags/default.nix
+++ users/brian/modules/hyprland/ags/default.nix
@@ -7,6 +7,19 @@
   home.packages = with pkgs; [
     bun
     dart-sass
+    fd
+    brightnessctl
+    swww
+    inputs.matugen.packages.${system}.default
+    slurp
+    wf-recorder
+    wl-clipboard
+    wayshot
+    swappy
+    hyprpicker
+    pavucontrol
+    networkmanager
+    gtk3
   ];
 
   programs.ags = {
