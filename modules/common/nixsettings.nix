fae63d2a929297c453fa729132490213a1a48c33
Brian Semrad <bjsemrad@gmail.com>
2024-07-23 20:12:20 -0500

    Updates

--- modules/common/nixsettings.nix
+++ modules/common/nixsettings.nix
@@ -1,7 +1,7 @@
 {
   nix.gc.automatic = true;
   nix.gc.dates = "daily";
-  nix.gc.options = "--delete-older-than +5";
+  nix.gc.options = "--delete-older-than 7d";
   nix.optimise.automatic = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
