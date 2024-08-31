fa04513219b76d11b5944ac2571b1489c65461c9
Brian Semrad <bjsemrad@gmail.com>
2024-08-31 17:27:07 -0500

    0.42.0 rollback

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       type = "git"; 
       url = "https://github.com/hyprwm/Hyprland";
       submodules = true;
-      ref = "refs/tags/v0.42.0";
+      ref = "refs/tags/v0.41.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
