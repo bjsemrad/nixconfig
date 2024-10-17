f783f5639558e5dfd9ac1c35a1fcb5e15dc8a353
Brian Semrad <bjsemrad@gmail.com>
2024-10-16 20:24:58 -0500

    Clean up and settings

--- users/brian/modules/shell/default.nix
+++ users/brian/modules/shell/default.nix
@@ -46,6 +46,8 @@ let
           local current_dir=$PWD
           if [[ $current_dir == $HOME ]]; then
               current_dir="~"
+          else
+              current_dir=''\${current_dir##*/}
           fi
           command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
         fi
