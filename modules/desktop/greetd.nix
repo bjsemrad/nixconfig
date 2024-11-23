8e47f164922fe158157ca14bbc78e240afb451d5
Brian Semrad <bjsemrad@gmail.com>
2024-11-23 09:39:22 -0600

    Reset

--- modules/desktop/greetd.nix
+++ modules/desktop/greetd.nix
@@ -6,7 +6,7 @@
     vt = 8;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session --asterisks";
         user = "greeter";
       };
     };
