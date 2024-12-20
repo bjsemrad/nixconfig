e31e9e95b8814365f467f38d1521c5135762e799
Brian Semrad <bjsemrad@gmail.com>
2024-12-19 21:43:20 -0600

    Git config updates

--- users/brian/modules/git/default.nix
+++ users/brian/modules/git/default.nix
@@ -3,5 +3,16 @@
     enable = true;
     userName = "Brian Semrad";
     userEmail = "bjsemrad@gmail.com";
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      pull = {
+        rebase = true;
+      };
+    };
   };
 }
