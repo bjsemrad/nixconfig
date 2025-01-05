fa8e93f2982fdd0aa377bca1babe7e94f51d4981
Brian Semrad <bjsemrad@gmail.com>
2025-01-04 18:15:59 -0600

    Tmux status bar tweaking

--- users/brian/modules/tmux/default.nix
+++ users/brian/modules/tmux/default.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
    home.packages = with pkgs; [ tmux-sessionizer ];
+   home.file = {
+        ".config/tms/config.toml".source = ./config.toml;
+   };
 
   programs.tmux = {
     enable = true;
@@ -24,13 +27,7 @@
           set -g @continuum-save-interval '1' # minutes
         '';
       }
-      {
-        plugin = tmuxPlugins.onedark-theme;
-        extraConfig = ''
-          set -g @onedark_time_format "%I:%M %p"
-          set -g @onedark_date_format "%m/%d/%y"
-        '';
-      }
+      tmuxPlugins.prefix-highlight
       tmuxPlugins.tmux-fzf
     ];
 
@@ -41,23 +38,21 @@
 
       set -g prefix C-a
       unbind-key t
-
-      bind-key -T prefix t switch-client -T tms
-      bind-key -T prefix p display-popup -E "tms"
-      bind-key -T tms p display-popup -E "tms"
-      bind-key -T prefix S display-popup -E "tms switch"
-      bind-key -T tms s display-popup -E "tms switch"
-
-      bind-key -T prefix w switch-client -T window
-      bind-key -T window c new-window
-      bind-key -T window r command-prompt -I "#W" { rename-window "%%" } 
-      bind-key -T window x confirm kill-window
-
+ 
+      #Session
       bind-key -T prefix s switch-client -T session
+      bind-key -T session s display-popup -E "tms switch"
+      bind-key -T session p display-popup -E "tms"
       bind-key -T session r command-prompt -p "Rename active session to: " "run-shell 'tms rename %1'"
-      bind-key -T session c command-prompt -p "New Session:" "new-session -A -s '%%'"
+      bind-key -T session n command-prompt -p "New Session:" "new-session -A -s '%%'"
       bind-key -T session x confirm "run-shell 'tms kill'"
 
+      #Window
+      bind-key -T prefix w switch-client -T window
+      bind-key -T window n new-window
+      bind-key -T window r command-prompt -I "#W" { rename-window "%%" } 
+
+      #PANE
       bind-key -T prefix p switch-client -T pane
       bind-key -T pane h split-window -h
       bind-key -T pane v split-window -v
@@ -68,6 +63,53 @@
       set -g mouse on
 
       set -g status-position bottom
+
+
+# Set the One Dark theme colors as tmux variables
+set -g @onedark_black "#282c34"
+set -g @onedark_blue "#61afef"
+set -g @onedark_yellow "#e5c07b"
+set -g @onedark_red "#e06c75"
+set -g @onedark_white "#aab2bf"
+set -g @onedark_green "#98c379"
+set -g @onedark_visual_grey "#3e4452"
+set -g @onedark_comment_grey "#5c6370"
+
+# Set status bar appearance
+set -g status on
+set -g status-justify left
+set -g status-left-length 100
+set -g status-right-length 100
+
+
+# Set window style colors
+set -g window-style "fg=$onedark_comment_grey"
+set -g window-active-style "fg=$onedark_white"
+
+# Set display panes colors
+set -g display-panes-active-colour "$onedark_yellow"
+set -g display-panes-colour "$onedark_blue"
+
+# Set the status bar colors
+set -g status-bg "$onedark_black"
+set -g status-fg "$onedark_white"
+
+# Prefix highlight settings
+set -g @prefix_highlight_fg "$onedark_black"
+set -g @prefix_highlight_bg "$onedark_green"
+set -g @prefix_highlight_copy_mode_attr "fg=$onedark_black,bg=$onedark_green"
+set -g @prefix_highlight_output_prefix "  "
+
+# Set status-right to include time, date, and custom widgets
+set -g status-right "#[fg=$onedark_green,bg=$onedark_black,nobold,nounderscore,noitalics]#[fg=$onedark_black,bg=$onedark_green,bold] #h "
+
+# Set status-left to show session and prefix highlight
+set -g status-left "#[fg=$onedark_black,bg=$onedark_green,bold] #S #[fg=$onedark_green,bg=$onedark_black,nobold,nounderscore,noitalics]"
+
+# Set window status format
+set -g window-status-format "#[fg=$onedark_black,bg=$onedark_black,nobold,nounderscore,noitalics]#[fg=$onedark_white,bg=$onedark_black] #I  #W #[fg=$onedark_black,bg=$onedark_black,nobold,nounderscore,noitalics]"
+set -g window-status-current-format "#[fg=$onedark_black,bg=$onedark_visual_grey,nobold,nounderscore,noitalics]#[fg=$onedark_white,bg=$onedark_visual_grey,nobold] #I  #W #[fg=$onedark_visual_grey,bg=$onedark_black,nobold,nounderscore,noitalics]"
+
     '';
   };
 }
