3ed7d6b1e85f544d1deccec96d887c513cd4d7ef
Brian Semrad <bjsemrad@gmail.com>
2025-07-11 10:03:50 -0500

    auto-login

--- users/brian/modules/hyprland/waybar/default.nix
+++ users/brian/modules/hyprland/waybar/default.nix
@@ -1,5 +1,4 @@
-{ osConfig, ... }:
-{
+{ osConfig, ... }: {
 
   home.file.".config/waybar/scripts/recorder.sh" = {
     source = ./scripts/recorder.sh;
@@ -15,16 +14,12 @@
         spacing = 0;
         layer = "top";
         gtk-layer-shell = true;
-        margin-top =  0; #5;
-        margin-left = 0; #10;
-        margin-right = 0; #10;
+        margin-top = 0; # 5;
+        margin-left = 0; # 10;
+        margin-right = 0; # 10;
         margin-bottom = 5;
-        modules-left = [
-          "hyprland/workspaces"
-        ];
-        modules-center = [
-          "hyprland/window"
-        ];
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "hyprland/window" ];
         modules-right = [
           #"custom/recorder"
           "custom/clipboard"
@@ -32,13 +27,11 @@
           "network"
           "pulseaudio#sink"
           "pulseaudio#source"
-       ] ++ ( 
-        if (osConfig.networking.hostName == "thor") then
-          [ "battery" 
-	    "clock"
-	  ]
-        else [ "clock" ]
-      );
+        ] ++ (if (osConfig.networking.hostName == "thor") then [
+          "battery"
+          "clock"
+        ] else
+          [ "clock" ]);
         "wlr/taskbar" = {
           "format" = "{icon}";
           "icon-size" = 14;
@@ -49,17 +42,11 @@
         "custom/launcher" = {
           format = "{icon}";
           "icon-size" = 22;
-          format-icons = [
-            ""
-          ];
+          format-icons = [ "" ];
           "on-click" = "$HOME/.config/rofi/scripts/launcher.sh";
         };
-        "custom/sep" = {
-          "format" = "|";
-        };
-        "custom/space" = {
-          "format" = " ";
-        };
+        "custom/sep" = { "format" = "|"; };
+        "custom/space" = { "format" = " "; };
         "hyprland/window" = {
           "format" = "{title}";
           "max-length" = 50;
@@ -67,9 +54,7 @@
         };
         "custom/clipboard" = {
           format = "{icon}";
-          format-icons = {
-            "default" = "󰨸";
-          };
+          format-icons = { "default" = "󰨸"; };
           "on-click" = "$HOME/.config/rofi/scripts/clipboard.sh";
           "on-click-right" = "cliphist wipe";
         };
@@ -95,23 +80,20 @@
           "return-type" = "json";
           "signal" = 12;
           "interval" = 5;
-          "on-click" = "wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4')";
+          "on-click" =
+            "wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4')";
           "on-click-right" = "pkill --signal SIGINT wf-recorder";
           "escape" = true;
         };
         "custom/session" = {
           format = "{icon}";
-          format-icons = {
-            "default" = "󰐥 ";
-          };
+          format-icons = { "default" = "󰐥 "; };
           "toolip" = false;
           "on-click" = "killall wlogout || wlogout -b 5 -m 500";
         };
         "backlight" = {
           format = "{icon}";
-          format-icons = [
-            ""
-          ];
+          format-icons = [ "" ];
           "tooltip-format" = "{icon}  {percent}%";
         };
         "hyprland/workspaces" = {
@@ -119,9 +101,7 @@
           "active-only" = false;
           "on-click" = "activate";
           "format" = "{icon}";
-          "persistent-workspaces" = {
-            "*" = 2;
-          };
+          "persistent-workspaces" = { "*" = 2; };
           format-icons = {
             "1" = "1";
             "2" = "2";
@@ -145,17 +125,13 @@
           #        "format" = "{:%a %b %d %I:%M %p} ";
           "format" = "{:%a %I:%M %p} ";
           "interval" = 60;
-          "tooltip-format" = "<big>{:%a %b %d}</big>\n<tt><small>{calendar}</small></tt>";
+          "tooltip-format" = ''
+            <big>{:%a %b %d}</big>
+            <tt><small>{calendar}</small></tt>'';
         };
-        "cpu" = {
-          "format" = "  {usage}%";
-        };
-        "temperature" = {
-          "format" = " HEEH {temperatureC}°C";
-        };
-        "memory" = {
-          "format" = "  {}%";
-        };
+        "cpu" = { "format" = "  {usage}%"; };
+        "temperature" = { "format" = " HEEH {temperatureC}°C"; };
+        "memory" = { "format" = "  {}%"; };
         "bluetooth" = {
           "format" = "󰂯";
           "format-disabled" = "󰂲";
@@ -175,32 +151,21 @@
           "format-plugged" = " ";
           #"format-alt" = "{icon} {time}";
           "tooltip-format" = "{capacity}%, {time}";
-          "format-icons" = [
-            ""
-            ""
-            ""
-            ""
-            ""
-          ];
+          "format-icons" = [ "" "" "" "" "" ];
         };
-        "battery#bat2" = {
-          "bat" = "BAT2";
-        };
+        "battery#bat2" = { "bat" = "BAT2"; };
         "network" = {
-           "format-icons" = [
-            "󰤯"
-            "󰤟"
-            "󰤢"
-            "󰤥"
-            "󰤨"
-          ];
+          "format-icons" = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
           "family" = "ipv4";
           "format" = "{icon}";
           "format-wifi" = "{icon}";
           "format-ethernet" = "";
           "format-disconnected" = "󰖪";
-          "tooltip-format-wifi" = "{essid} {ipaddr} ({frequency} GHz)\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
-          "tooltip-format-ethernet" = "{ipaddr} ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+          "tooltip-format-wifi" = ''
+            {essid} {ipaddr} ({frequency} GHz)
+            ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}'';
+          "tooltip-format-ethernet" =
+            "{ipaddr} ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
           "tooltip-format-disconnected" = "Disconnected";
           "interval" = 3;
           "nospacing" = 1;
@@ -213,7 +178,9 @@
           "format-bluetooth-muted" = "";
           "format-muted" = "";
           "format-source" = "{volume}% ";
-          "tooltip-format" = "{desc}\n{volume}%";
+          "tooltip-format" = ''
+            {desc}
+            {volume}%'';
           "format-icons" = {
             "headphone" = "";
             "hands-free" = "";
@@ -221,11 +188,7 @@
             "phone" = "";
             "portable" = "";
             "car" = "";
-            "default" = [
-              ""
-              ""
-              ""
-            ];
+            "default" = [ "" "" "" ];
           };
           "on-click" = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
           "on-click-right" = "pavucontrol";
@@ -252,170 +215,171 @@
       };
     };
     style = ''
-            @define-color black        #0e1013; /* rgba(14, 16, 19, 1) */
-            @define-color bgDark       #1E2127; /* rgba(30, 33, 39, 1) */
-	    @define-color bg0          #1f2329;
-	    @define-color bg1          #282c34;
-	    @define-color bg2          #30363f;
-	    @define-color bg3          #323641;
-	    @define-color bg_d         #181b20;
-	    @define-color bg_blue      #61afef;
-	    @define-color bg_yellow    #e8c88c;
-	    @define-color fg           #a0a8b7;
-            @define-color fg_dark      #abb2bf;
-	    @define-color purple       #bf68d9;
-	    @define-color green        #8ebd6b;
-	    @define-color orange       #cc9057;
-	    @define-color blue         #4fa6ed;
-	    @define-color yellow       #e2b86b;
-	    @define-color cyan         #48b0bd;
-	    @define-color red          #e55561;
-	    @define-color grey         #535965;
-	    @define-color light_grey   #7a818e;
-	    @define-color dark_cyan    #266269;
-	    @define-color dark_red     #8b3434;
-	    @define-color dark_yellow  #835d1a;
-	    @define-color dark_purple  #7e3992;
-	    @define-color diff_add     #272e23;
-            @define-color diff_delete  #2d2223;
-            @define-color diff_change  #172a3a;
-	    @define-color diff_text    #274964;
+                  @define-color black        #0e1013; /* rgba(14, 16, 19, 1) */
+                  @define-color bgDark       #1E2127; /* rgba(30, 33, 39, 1) */
+      	    @define-color bg0          #1f2329;
+      	    @define-color bg1          #282c34;
+      	    @define-color bg2          #30363f;
+      	    @define-color bg3          #323641;
+      	    @define-color bg_d         #181b20;
+      	    @define-color bg_blue      #61afef;
+      	    @define-color bg_yellow    #e8c88c;
+      	    @define-color fg           #a0a8b7;
+                  @define-color fg_dark      #abb2bf;
+      	    @define-color purple       #bf68d9;
+      	    @define-color green        #8ebd6b;
+      	    @define-color orange       #cc9057;
+      	    @define-color blue         #4fa6ed;
+      	    @define-color yellow       #e2b86b;
+      	    @define-color cyan         #48b0bd;
+      	    @define-color red          #e55561;
+      	    @define-color grey         #535965;
+      	    @define-color light_grey   #7a818e;
+      	    @define-color dark_cyan    #266269;
+      	    @define-color dark_red     #8b3434;
+      	    @define-color dark_yellow  #835d1a;
+      	    @define-color dark_purple  #7e3992;
+      	    @define-color diff_add     #272e23;
+                  @define-color diff_delete  #2d2223;
+                  @define-color diff_change  #172a3a;
+      	    @define-color diff_text    #274964;
 
-            * {
-              font-family: Hack Nerd Font;
-              font-size: 14px;
-            }
+                  * {
+                    font-family: Hack Nerd Font;
+                    font-size: 14px;
+                  }
 
-            window {
-            }
+                  window {
+                  }
 
-            window#waybar {
-              /*background-color: rgba(31, 35, 41, 1.0);*/
-              background-color: rgba(14, 16, 19, 1.0);
-              transition-property: background-color;
-              transition-duration: 0.5s;
-              border-bottom: 2px solid @bg1;
-            }
+                  window#waybar {
+                    /*background-color: rgba(31, 35, 41, 1.0);*/
+                    background-color: rgba(14, 16, 19, 1.0);
+                    transition-property: background-color;
+                    transition-duration: 0.5s;
+                    border-bottom: 2px solid @bg1;
+                  }
 
-            .modules-right {
-              background: transparent;
-            }
+                  .modules-right {
+                    background: transparent;
+                  }
 
-            .modules-center {
-              background: transparent;
-            }
+                  .modules-center {
+                    background: transparent;
+                  }
 
-            .modules-left {
-              background: transparent;
-            }
+                  .modules-left {
+                    background: transparent;
+                  }
 
-            tooltip {
-              background-color: @black;
-              border: 2px solid @bg1;
-              color: @fg_dark;
-              border-radius: 10px;
-            }
-            
-            tooltip label {
-              color: @fg_dark;
-            }
+                  tooltip {
+                    background-color: @black;
+                    border: 2px solid @bg1;
+                    color: @fg_dark;
+                    border-radius: 10px;
+                  }
+                  
+                  tooltip label {
+                    color: @fg_dark;
+                  }
 
-            #workspaces {
-              margin-left: 10px;
-              background: transparent;
-            }
+                  #workspaces {
+                    margin-left: 10px;
+                    background: transparent;
+                  }
 
-            #workspaces button {
-                background: transparent;
-                color: @light_grey;
-                font-weight: bold;
-                border: none;
-            }
+                  #workspaces button {
+                      background: transparent;
+                      color: @light_grey;
+                      font-weight: bold;
+                      border: none;
+                  }
 
-            #workspaces button:hover {
-                color: @fg;
-                background: transparent;
-            }
+                  #workspaces button:hover {
+                      color: @fg;
+                      font-weight: bold;
+                      border: none;
+                      background: transparent;
+                  }
 
-            #workspaces button.active {
-              color: @black;
-              background: @fg;
-              border-radius: 5px;
-              border: 2px solid @bg1;
-              margin: 1px 0 2px 0;
-              /*background: transparent;*/
-            }
+                  #workspaces button.active {
+                    color: @black;
+                    background: @fg;
+                    border-radius: 5px;
+                    border: 2px solid @bg1;
+                    /*background: transparent;*/
+                  }
 
-            #taskbar button {
-              background: transparent;
-              border: none;
-            }
+                  #taskbar button {
+                    background: transparent;
+                    border: none;
+                  }
 
-            #taskbar button.active {
-              background: transparent;
-            }
+                  #taskbar button.active {
+                    background: transparent;
+                  }
 
-            button.urgent {
-                color: @red;
-                background: @black;
-                border-radius: 10px;
-            }
+                  button.urgent {
+                      color: @red;
+                      background: @black;
+                      border-radius: 10px;
+                  }
 
-            #custom-sep {
-              color: @black;
-            }
+                  #custom-sep {
+                    color: @black;
+                  }
 
-            #custom-session {
-              color: @text;
-              font-size: 18px;
-            }
+                  #custom-session {
+                    color: @text;
+                    font-size: 18px;
+                  }
 
-            #window {
-              color: @fg_dark;
-            }
+                  #window {
+                    color: @fg_dark;
+                  }
 
-            #clock {
-              margin-right: 10px;
-            }
+                  #clock {
+                    margin-right: 10px;
+                  }
 
-            #pulseaudio,
-            #wireplumber,
-            #battery,
-            #custom-clipboard,
-            #custom-menu,
-            #backlight,
-            #idle_inhibitor,
-            #network,
-            #clock,
-            #trayu
-            #custom-session,
-            #custom-recorder,
-            #bluetooth,
-            #cpu,
-            #disk,
-            #memory,
-            #temperature {
-              color: @fg_dark;
-              min-width: 13px;
-              margin-right: 10px;
-              padding: 0px 5px 0px 5px;
-            }
+                  #pulseaudio,
+                  #wireplumber,
+                  #battery,
+                  #custom-clipboard,
+                  #custom-menu,
+                  #backlight,
+                  #idle_inhibitor,
+                  #network,
+                  #clock,
+                  #trayu
+                  #custom-session,
+                  #custom-recorder,
+                  #bluetooth,
+                  #cpu,
+                  #disk,
+                  #memory,
+                  #temperature {
+                    color: @fg_dark;
+                    min-width: 13px;
+                    margin-right: 10px;
+                    padding: 0px 5px 0px 5px;
+                  }
 
-            #bluetooth.connected {
-              color: @green;
-            }
+                  #bluetooth.connected {
+                    color: @green;
+                  }
 
-            #battery.warning:not(.charging) {
-              color: @orange;
-            }
-            
-            #battery.critical:not(.charging) {
-              color: @red;
-            }
+                  #battery.warning:not(.charging) {
+                    color: @orange;
+                  }
+                  
+                  #battery.critical:not(.charging) {
+                    color: @red;
+                  }
 
-            #custom-recorder.enabled {
-              color: @green;
-            }
+                  #custom-recorder.enabled {
+                    color: @green;
+                  }
     '';
   };
 }
