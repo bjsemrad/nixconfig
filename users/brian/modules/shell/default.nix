ba7c6894af924f25baab92e61a8718751a15d78b
Brian Semrad <bjsemrad@gmail.com>
2025-07-10 21:57:50 -0500

    updates

--- users/brian/modules/shell/default.nix
+++ users/brian/modules/shell/default.nix
@@ -1,8 +1,6 @@
 { pkgs, config, ... }:
-let 
-  nixconfigdir = "nixconfig";
-  in
-{
+let nixconfigdir = "nixconfig";
+in {
   programs.carapace = {
     enable = true;
     enableNushellIntegration = true;
@@ -13,8 +11,13 @@ let
     enable = true;
     shellAliases = {
       #homeupdate = "home-manager switch --flake ${nixconfigdir}";
-      nixupdate = "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
-      nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixupdate =
+        "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --option keep-outputs false --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      #forces cleanup of home manager generations
+      nixupdateclean =
+        "(cd ${config.home.homeDirectory}/${nixconfigdir} && nix flake update) && sudo nixos-rebuild switch --verbose --upgrade --option keep-outputs false --option keep-derivations false --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixswitch =
+        "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
       nixgc = "sudo nix-store --gc";
       # recommeneded to sometimes run as sudo to collect additional garbage
       nixgcd = "sudo nix-collect-garbage -d";
@@ -27,69 +30,77 @@ let
       gp = "git push";
       gco = "git checkout";
       gst = "git status";
-      sshdash="ssh dash@dashboard.otter-rigel.ts.net";
-      sshprox="ssh root@proxmox.otter-rigel.ts.net";
-      sshtruenas="ssh root@10.0.10.13";
-      sshchannels="ssh channels@channels.otter-rigel.ts.net";
-      sshminecraft="ssh mine@10.0.10.11";
-      sshproxy="ssh nginx@proxy.otter-rigel.ts.net";
-      sshcroc="ssh worm@loki.otter-rigel.ts.net";
-      croc-rebuild="nixos-rebuild switch -s --flake .#loki --target-host root@loki.otter-rigel.ts.net  --verbose";
-      dashboard-rebuild="nixos-rebuild switch -s --flake .#tyr --target-host root@dashboard.otter-rigel.ts.net  --verbose";
-      proxy-rebuild="nixos-rebuild switch -s --flake .#yggdrasil --target-host root@proxy.otter-rigel.ts.net --verbose";
-      tailreceive="sudo tailscale file get .";
-      setup-vial="qmk setup bjsemrad/vial-qmk -H ~/vial-qmk -b vial";
-      setup-qmk="qmk setup bjsemrad/qmk_firmware -H ~/qmk_firmwarel";
-      build-lily58="QMK_HOME=~/vial-qmk qmk compile -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
-      flash-lily58="QMK_HOME=~/vial-qmk qmk flash -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
-      build-ferrismx="QMK_HOME=~/vial-qmk qmk compile -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
-      flash-ferrismx="QMK_HOME=~/vial-qmk qmk flash -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
+      sshdash = "ssh dash@dashboard.otter-rigel.ts.net";
+      sshprox = "ssh root@proxmox.otter-rigel.ts.net";
+      sshtruenas = "ssh root@10.0.10.13";
+      sshchannels = "ssh channels@channels.otter-rigel.ts.net";
+      sshminecraft = "ssh mine@10.0.10.11";
+      sshproxy = "ssh nginx@proxy.otter-rigel.ts.net";
+      sshcroc = "ssh worm@loki.otter-rigel.ts.net";
+      croc-rebuild =
+        "nixos-rebuild switch -s --flake .#loki --target-host root@loki.otter-rigel.ts.net  --verbose";
+      dashboard-rebuild =
+        "nixos-rebuild switch -s --flake .#tyr --target-host root@dashboard.otter-rigel.ts.net  --verbose";
+      proxy-rebuild =
+        "nixos-rebuild switch -s --flake .#yggdrasil --target-host root@proxy.otter-rigel.ts.net --verbose";
+      tailreceive = "sudo tailscale file get .";
+      setup-vial = "qmk setup bjsemrad/vial-qmk -H ~/vial-qmk -b vial";
+      setup-qmk = "qmk setup bjsemrad/qmk_firmware -H ~/qmk_firmwarel";
+      build-lily58 =
+        "QMK_HOME=~/vial-qmk qmk compile -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
+      flash-lily58 =
+        "QMK_HOME=~/vial-qmk qmk flash -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
+      build-ferrismx =
+        "QMK_HOME=~/vial-qmk qmk compile -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
+      flash-ferrismx =
+        "QMK_HOME=~/vial-qmk qmk flash -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
 
     };
-    autosuggestion.enable = true;   
+    autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     initContent = ''
-      export PATH=$PATH:~/tools
-      if uwsm check may-start && uwsm select; then
-	exec systemd-cat -t uwsm_start uwsm start default
-      fi
+            export PATH=$PATH:~/tools
+            if uwsm check may-start && uwsm select; then
+      	exec systemd-cat -t uwsm_start uwsm start default
+            fi
 
-      if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-        tmux attach-session -t main || tmux new-session -s main
-      fi
+            if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+              tmux attach-session -t main || tmux new-session -s main
+            fi
 
-      zellij_tab_name_update() {
-        if [[ -n $ZELLIJ ]]; then
-          local current_dir=$PWD
-          if [[ $current_dir == $HOME ]]; then
-              current_dir="~"
-          else
-              current_dir=''\${current_dir##*/}
-          fi
-          command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
-        fi
-      }
+            zellij_tab_name_update() {
+              if [[ -n $ZELLIJ ]]; then
+                local current_dir=$PWD
+                if [[ $current_dir == $HOME ]]; then
+                    current_dir="~"
+                else
+                    current_dir=''${current_dir##*/}
+                fi
+                command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+              fi
+            }
 
 
-      #if [[ -z "$ZELLIJ" ]]; then
-      #  zellij attach -c main
+            #if [[ -z "$ZELLIJ" ]]; then
+            #  zellij attach -c main
 
-       # if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
-       #   exit
-       # fi
-      #fi
+             # if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+             #   exit
+             # fi
+            #fi
 
-      #zellij_tab_name_update
-      #chpwd_functions+=(zellij_tab_name_update)
-      # This command let's me execute arbitrary binaries downloaded through channels such as mason.
-      export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
+            #zellij_tab_name_update
+            #chpwd_functions+=(zellij_tab_name_update)
+            # This command let's me execute arbitrary binaries downloaded through channels such as mason.
+            export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
     '';
   };
 
   programs.nushell = {
     enable = true;
-    envFile.text = ''$env.PATH = ($env.PATH | split row (char esep) | append '~/tools/')'';
+    envFile.text =
+      "$env.PATH = ($env.PATH | split row (char esep) | append '~/tools/')";
     extraConfig = ''
       $env.config = {
          show_banner: false,
@@ -135,7 +146,8 @@ let
 
     '';
     shellAliases = {
-      nixswitch = "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
+      nixswitch =
+        "sudo nixos-rebuild switch --verbose --upgrade --flake ${config.home.homeDirectory}/${nixconfigdir}";
       nixgc = "sudo nix-store --gc";
       # recommeneded to sometimes run as sudo to collect additional garbage
       nixgcd = "sudo nix-collect-garbage -d";
@@ -148,21 +160,27 @@ let
       gp = "git push";
       gco = "git checkout";
       gst = "git status";
-      sshdash="ssh dash@dashboard.otter-rigel.ts.net";
-      sshprox="ssh root@proxmox.otter-rigel.ts.net";
-      sshtruenas="ssh root@10.0.10.13";
-      sshchannels="ssh channels@channels.otter-rigel.ts.net";
-      sshminecraft="ssh mine@minecraft.otter-rigel.ts.net";
-      sshproxy="ssh nginx@proxy.otter-rigel.ts.net";
-      dashboard-rebuild="nixos-rebuild switch -s --flake .#tyr --target-host root@dashboard.otter-rigel.ts.net  --verbose";
-      proxy-rebuild="nixos-rebuild switch -s --flake .#yggdrasil --target-host root@proxy.otter-rigel.ts.net --verbose";
-      tailreceive="sudo tailscale file get .";
-      setup-vial="qmk setup bjsemrad/vial-qmk -H ~/vial-qmk -b vial";
-      setup-qmk="qmk setup bjsemrad/qmk_firmware -H ~/qmk_firmwarel";
-      build-lily58="QMK_HOME=~/vial-qmk qmk compile -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
-      flash-lily58="QMK_HOME=~/vial-qmk qmk flash -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
-      build-ferrismx="QMK_HOME=~/vial-qmk qmk compile -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
-      flash-ferrismx="QMK_HOME=~/vial-qmk qmk flash -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
+      sshdash = "ssh dash@dashboard.otter-rigel.ts.net";
+      sshprox = "ssh root@proxmox.otter-rigel.ts.net";
+      sshtruenas = "ssh root@10.0.10.13";
+      sshchannels = "ssh channels@channels.otter-rigel.ts.net";
+      sshminecraft = "ssh mine@minecraft.otter-rigel.ts.net";
+      sshproxy = "ssh nginx@proxy.otter-rigel.ts.net";
+      dashboard-rebuild =
+        "nixos-rebuild switch -s --flake .#tyr --target-host root@dashboard.otter-rigel.ts.net  --verbose";
+      proxy-rebuild =
+        "nixos-rebuild switch -s --flake .#yggdrasil --target-host root@proxy.otter-rigel.ts.net --verbose";
+      tailreceive = "sudo tailscale file get .";
+      setup-vial = "qmk setup bjsemrad/vial-qmk -H ~/vial-qmk -b vial";
+      setup-qmk = "qmk setup bjsemrad/qmk_firmware -H ~/qmk_firmwarel";
+      build-lily58 =
+        "QMK_HOME=~/vial-qmk qmk compile -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
+      flash-lily58 =
+        "QMK_HOME=~/vial-qmk qmk flash -kb lily58/rev1 -km brian -e CONVERT_TO=rp2040_ce";
+      build-ferrismx =
+        "QMK_HOME=~/vial-qmk qmk compile -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
+      flash-ferrismx =
+        "QMK_HOME=~/vial-qmk qmk flash -kb ferris/sweep -km mxferris_linvert_blok -e CONVERT_TO=blok";
     };
   };
 }
