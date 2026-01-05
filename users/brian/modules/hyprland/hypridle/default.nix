55ddb0fe0830f23ba401710fe34d7f728649b516
Brian Semrad <bjsemrad@gmail.com>
2026-01-04 21:26:59 -0600

    minor fixes and updates

--- users/brian/modules/hyprland/hypridle/default.nix
+++ users/brian/modules/hyprland/hypridle/default.nix
@@ -1,4 +1,10 @@
-{ config, inputs, pkgs, osConfig, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  osConfig,
+  ...
+}:
 {
   # imports = [
   # hypridle.homeManagerModules.default
@@ -11,35 +17,72 @@
       listener = [
         {
           timeout = 400;
-          on-timeout = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms off";
-          on-resume = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms on";
+          on-timeout = "[ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+            inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+          }/bin/hyprctl dispatch dpms off || ${
+            inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+          }/bin/niri msg action power-off-monitors";
+          on-resume = "[ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+            inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+          }/bin/hyprctl dispatch dpms on || ${
+            inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+          }/bin/niri msg action power-on-monitors";
         }
-      ] ++ (
+      ]
+      ++ (
         if (osConfig.networking.hostName == "thor") then
-          [{
-            timeout = 300;
-            on-timeout = "${inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock}/bin/hyprlock";
-          }
+          [
+            {
+              timeout = 300;
+              on-timeout = "${inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock}/bin/hyprlock";
+            }
             {
               timeout = 600;
-              on-timeout = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms off && ${pkgs.systemd}/bin/systemctl suspend";
-              on-resume = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms on";
-            }]
-        else [{
+              on-timeout = "([ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+                inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+              }/bin/hyprctl dispatch dpms off || ${
+                inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+              }/bin/niri msg action power-off-monitors) && ${pkgs.systemd}/bin/systemctl suspend";
+
+              on-resume = "[ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+                inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+              }/bin/hyprctl dispatch dpms on || ${
+                inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+              }/bin/niri msg action power-on-monitors";
+            }
+          ]
+        else
+          [
+            {
               timeout = 400;
               on-timeout = "${inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock}/bin/hyprlock";
-             }
-             {
-              timeout = 600;
-              on-timeout = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms off && ${pkgs.systemd}/bin/systemctl suspend";
-              on-resume = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms on && uwsm app -- openrgb -p Blue";
             }
-            ]
+            {
+              timeout = 600;
+              on-timeout = "([ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+                inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+              }/bin/hyprctl dispatch dpms off || ${
+                inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+              }/bin/niri msg action power-off-monitors) && ${pkgs.systemd}/bin/systemctl suspend";
+
+              on-resume = "([ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+                inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+              }/bin/hyprctl dispatch dpms on || ${
+                inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+              }/bin/niri msg action power-on-monitors) && uwsm app -- openrgb -p Blue";
+            }
+          ]
       );
       general = {
         lock_cmd = "${inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock}/bin/hyprlock";
-        before_sleep_cmd = "${inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock}/bin/hyprlock";
-        after_sleep_cmd = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms on";
+        before_sleep_cmd = "${
+          inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock
+        }/bin/hyprlock";
+        after_sleep_cmd = "[ -n \"$${HYPRLAND_INSTANCE_SIGNATURE:-}\" ] && ${
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+        }/bin/hyprctl dispatch dpms on || ${
+          inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri
+        }/bin/niri msg action power-on-monitors";
       };
     };
   };
