20d45ca37cc00bb00aeab1e0e3244473cf774f5d
Brian Semrad <bjsemrad@gmail.com>
2026-01-17 08:08:38 -0600

    hyprland upgrade

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -75,19 +75,19 @@
     # package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.ollama;
   };
 
-  services.open-webui = {
-    enable = true;
-    openFirewall = true;
-    port = 11435;
-    host = "0.0.0.0";
-    environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-    };
-  };
+  # services.open-webui = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   port = 11435;
+  #   host = "0.0.0.0";
+  #   environment = {
+  #     ANONYMIZED_TELEMETRY = "False";
+  #     DO_NOT_TRACK = "True";
+  #     SCARF_NO_ANALYTICS = "True";
+  #     OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+  #     OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+  #   };
+  # };
 
   networking = {
     interfaces = {
