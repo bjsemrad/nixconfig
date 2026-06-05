00501bdf15f677c9eca60f53fcac8d04f74618ba
Brian Semrad <bjsemrad@gmail.com>
2026-06-05 10:08:57 -0500

    updates

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -1,11 +1,17 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.noto
-    nerd-fonts.caskaydia-mono
-    nerd-fonts.hack
-    font-awesome
-    noto-fonts
-  ];
+  fonts = {
+    fontDir.enable = true;
+    fontconfig.enable = true;
+    enableDefaultPackages = true;
+
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.noto
+      nerd-fonts.caskaydia-mono
+      nerd-fonts.hack
+      font-awesome
+      noto-fonts
+    ];
+  };
 }
