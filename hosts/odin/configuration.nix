719a8851746065c8b55e355cb54ac5dbfd0dc690
Brian Semrad <bjsemrad@gmail.com>
2026-08-29 16:36:27 -0500

    nix updates

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -66,28 +66,29 @@
   services.ollama = {
     enable = true;
     host = "[::]";
+    package = pkgs.ollama-rocm;
     # acceleration = "rocm";
     # rocmOverrideGfx = "10.3.0";
-    # environmentVariables = {
-    # HSA_OVERRIDE_GFX_VERSION = "10.3.0";
-    # HCC_AMDGPU_TARGET = "gfx1030";
-    # };
+    environmentVariables = {
+      HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+      HCC_AMDGPU_TARGET = "gfx1030";
+    };
     # package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.ollama;
   };
 
-  # services.open-webui = {
-  #   enable = true;
-  #   openFirewall = true;
-  #   port = 11435;
-  #   host = "0.0.0.0";
-  #   environment = {
-  #     ANONYMIZED_TELEMETRY = "False";
-  #     DO_NOT_TRACK = "True";
-  #     SCARF_NO_ANALYTICS = "True";
-  #     OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-  #     OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-  #   };
-  # };
+  services.open-webui = {
+    enable = true;
+    openFirewall = true;
+    port = 11435;
+    host = "0.0.0.0";
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
+  };
 
   networking = {
     interfaces = {
