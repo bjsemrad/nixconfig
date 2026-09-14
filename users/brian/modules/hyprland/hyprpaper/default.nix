c4d3c3cb80124a23edc3a088b164cb0ec5006463
Brian Semrad <bjsemrad@gmail.com>
2026-09-13 20:07:50 -0500

    Fill wallpaper

--- users/brian/modules/hyprland/hyprpaper/default.nix
+++ users/brian/modules/hyprland/hyprpaper/default.nix
@@ -1,4 +1,4 @@
-{inputs, pkgs, ...}:
+{ inputs, pkgs, ... }:
 {
   services.hyprpaper = {
     enable = true;
@@ -14,10 +14,11 @@
       #   "/home/brian/.config/hypr/shaded_landscape.png"
       #   "/home/brian/.config/hypr/water-drop.jpg"
       # ];
-      wallpaper = [ 
-        { 
+      wallpaper = [
+        {
           monitor = "";
-          path ="/home/brian/.config/hypr/retropc.jpg"; 
+          path = "/home/brian/.config/hypr/retropc.jpg";
+          fit_mode = "fill";
         }
       ];
     };
@@ -25,53 +26,36 @@
   home.file = {
     #".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
     ".config/hypr/forrest.png".source = ../../../wallpaper/forrest.png;
-    ".config/hypr/small-memory.jpg".source =
-      ../../../wallpaper/small-memory.jpg;
+    ".config/hypr/small-memory.jpg".source = ../../../wallpaper/small-memory.jpg;
     ".config/hypr/nix-purple.png".source = ../../../wallpaper/nix-purple.png;
     ".config/hypr/fetchnix.png".source = ../../../wallpaper/fetchnix.png;
-    ".config/hypr/blue_purple_waves.jpg".source =
-      ../../../wallpaper/blue_purple_waves.jpg;
-    ".config/hypr/abstractglass.png".source =
-      ../../../wallpaper/abstractglass.png;
-    ".config/hypr/colorful-abstract.jpg".source =
-      ../../../wallpaper/colorful-abstract.jpg;
+    ".config/hypr/blue_purple_waves.jpg".source = ../../../wallpaper/blue_purple_waves.jpg;
+    ".config/hypr/abstractglass.png".source = ../../../wallpaper/abstractglass.png;
+    ".config/hypr/colorful-abstract.jpg".source = ../../../wallpaper/colorful-abstract.jpg;
     ".config/hypr/exploding.jpg".source = ../../../wallpaper/exploding.jpg;
-    ".config/hypr/vibrant-colors.jpg".source =
-      ../../../wallpaper/vibrant-colors.jpg;
-    ".config/hypr/multicolored.jpg".source =
-      ../../../wallpaper/multicolored.jpg;
-    ".config/hypr/flowing-mountain.jpg".source =
-      ../../../wallpaper/flowing-mountain.jpg;
+    ".config/hypr/vibrant-colors.jpg".source = ../../../wallpaper/vibrant-colors.jpg;
+    ".config/hypr/multicolored.jpg".source = ../../../wallpaper/multicolored.jpg;
+    ".config/hypr/flowing-mountain.jpg".source = ../../../wallpaper/flowing-mountain.jpg;
     ".config/hypr/water-drop.jpg".source = ../../../wallpaper/water-drop.jpg;
     ".config/hypr/abstract-wavy-background.jpg".source =
       ../../../wallpaper/abstract-wavy-background.jpg;
-    ".config/hypr/colorful-painting.jpg".source =
-      ../../../wallpaper/colorful-painting.jpg;
-    ".config/hypr/view-clear-water-motion.jpg".source =
-      ../../../wallpaper/view-clear-water-motion.jpg;
-    ".config/hypr/computer-chip-cpu.png".source =
-      ../../../wallpaper/computer-chip-cpu.png;
+    ".config/hypr/colorful-painting.jpg".source = ../../../wallpaper/colorful-painting.jpg;
+    ".config/hypr/view-clear-water-motion.jpg".source = ../../../wallpaper/view-clear-water-motion.jpg;
+    ".config/hypr/computer-chip-cpu.png".source = ../../../wallpaper/computer-chip-cpu.png;
     ".config/hypr/matrix.png".source = ../../../wallpaper/matrix.png;
     ".config/hypr/leaves.png".source = ../../../wallpaper/leaves.png;
     ".config/hypr/flower.png".source = ../../../wallpaper/flower.png;
-    ".config/hypr/shaded_landscape.png".source =
-      ../../../wallpaper/shaded_landscape.png;
+    ".config/hypr/shaded_landscape.png".source = ../../../wallpaper/shaded_landscape.png;
     ".config/hypr/forest-mist.png".source = ../../../wallpaper/forest-mist.png;
-    ".config/hypr/island-night-moon.png".source =
-      ../../../wallpaper/island-night-moon.png;
+    ".config/hypr/island-night-moon.png".source = ../../../wallpaper/island-night-moon.png;
     ".config/hypr/blue_night_moon_over_lake.png".source =
       ../../../wallpaper/blue_night_moon_over_lake.png;
-    ".config/hypr/forest-landscape.png".source =
-      ../../../wallpaper/forest-landscape.png;
-    ".config/hypr/mountain-lake.png".source =
-      ../../../wallpaper/mountain-lake.png;
+    ".config/hypr/forest-landscape.png".source = ../../../wallpaper/forest-landscape.png;
+    ".config/hypr/mountain-lake.png".source = ../../../wallpaper/mountain-lake.png;
     ".config/hypr/wallhaven3.png".source = ../../../wallpaper/wallhaven3.png;
-    ".config/hypr/fantasy-world.png".source =
-      ../../../wallpaper/fantasy_world.png;
-    ".config/hypr/cosmic-light.jpg".source =
-      ../../../wallpaper/cosmic-light.jpg;
-    ".config/hypr/minimalpattern.png".source =
-      ../../../wallpaper/minimalpattern.png;
+    ".config/hypr/fantasy-world.png".source = ../../../wallpaper/fantasy_world.png;
+    ".config/hypr/cosmic-light.jpg".source = ../../../wallpaper/cosmic-light.jpg;
+    ".config/hypr/minimalpattern.png".source = ../../../wallpaper/minimalpattern.png;
     ".config/hypr/funicons.jpg".source = ../../../wallpaper/funicons.jpg;
     ".config/hypr/binary.jpg".source = ../../../wallpaper/binary.jpg;
     ".config/hypr/programmer.jpg".source = ../../../wallpaper/programmer.jpg;
